a742232fd64c36bd2538cc52bb79952701563ad5
ekgeorgieva <ekaterinageor570@abv.bg>
2026-04-16 17:37:47 +0300

    First things of app
    include two main div. And sections. Wihout css.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
+import "./App.css";
+
 function App() {
   return (
-    <h1>Todo App</h1>
+    <div className="mainContainer">
+      <div className="pending">
+        <select className="Filter by:">
+          <option>Ekaterina</option>
+          <option>Bret</option>
+        </select>
+        <select className="sort">
+          <option>Title (asc)</option>
+          <option>Title (desc)</option>
+        </select>
+        <h2>Pending:</h2>
+      </div>
+      <div className="completed">
+      <select className="sortCompleted">
+          <option>Title (asc)</option>
+          <option>Title (desc)</option>
+        </select>
+        <h2>Completed:</h2>
+      </div>
+    </div>
   );
 }
+
+
+
 
 export default App;
